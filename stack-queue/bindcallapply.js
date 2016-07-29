7852e71c5f93e6046e12d2207954bfca0e770316
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-07-29 10:39:39 -0700

    duck typing

--- stack-queue/bindcallapply.js
+++ stack-queue/bindcallapply.js
@@ -7,42 +7,58 @@
 
 //http://www.nfriedly.com/techblog/2009/06/advanced-javascript-objects-arrays-and-array-like-objects/
 
+//Create a function that uses 'this'
+Person.all = [];
+
 function Person(name){
   this.name = name;
   this.sayName = function(){
     return ('my name is ' +this.name);
   }
+  Person.all.push(this);
+}
+Person.count = function(){
+  return this.all.length;
 }
 
+//Add a function to the prototype that uses this and a param
 Person.prototype.greet = function(who){
  return ('hi to ' + who + ' from ' + this.name);
 }
 
-//sayGoodBye is not on the prototype
-Person.sayGoodBye = function (){
-  //console.log(this);
-  return ('bye from ' + this.name);
-}
+//sayGoodBye is not on the prototype - static method
+// Person.sayGoodBye = function (){
+//   console.log('this',this);  //generally don't use 'this' in a class function
+//   return ('bye from ' + this.name);
+// }
+//
+
 
 console.log('>>>> execute functions');
 var person = new Person('George');
 console.log('Person:',person.greet('Bob'));
 console.log('Person:', person.sayName());
-//name of function will be output
-console.log('Person:',Person.sayGoodBye());
 
-//repurposing greet and sayGoodBye
+console.log('count', Person.count());
+
+//repurposing greet
 function Animal(name){
   this.name = name;
+  this.all =[];  //duck typing 'if it walks like a duck and talks like a duck'
 }
 //call
 console.log('>>>> execute .call')
 var animal = new Animal('Florence');
 
-console.log('Animal: ', person.greet.call(animal,'Heidi'));
-console.log('Animal: ', person.sayName.call(animal));
-//name of animal will be output
-console.log('Animal: ', Person.sayGoodBye.call(animal));
+console.log('call Animal: ', person.greet.call(animal,'Heidi'));
+console.log('call Animal: ', person.sayName.call(animal));
+
+//you can call class function but it's data is from Person
+console.log('call class count Animal: ', Person.count.call(animal));
+
+console.log('apply Animal: ', person.greet.apply(animal,['Heidi']));
+console.log('applyAnimal: ', person.sayName.apply(animal));
+
 
 //  bind
 console.log('>>>> execute with bind')
@@ -53,8 +69,8 @@ console.log('bind greet:',person.greet.bind(animal,'Josh')());
 
 function List(){
   this.alist = [];
-  this.addToList = function(arr){
-    this.alist.push(arr);
+  this.addToList = function(item){
+    this.alist.push(item);
     return this.alist;
   }
 
